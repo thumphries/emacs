c65e2110abd3794f75049d1cc5d21ebab37150e1
Tim Humphries <tim.humphries@ambiata.com>
2016-11-16 17:36:41 +1100

    Add licensing info

--- emacs/.emacs.d/mafia/mafia.el
+++ emacs/.emacs.d/mafia/mafia.el
@@ -1,4 +1,27 @@
 ;;; mafia.el
+;; this is entirely the work of chris done et al
+
+;; Copyright (c) 2016 Chris Done
+;; Copyright (c) 2015 Athur Fayzrakhmanov
+;; Copyright (c) 2013 Herbert Valerio Riedel
+;; Copyright (c) 2007 Stefan Monnier
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
 
 ;; not sure which of these is needed
 ;;(require 'json)
